b78bdf2faef1a63aba55bfcdd1f1f5299dff199b
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-02-09 19:33:35 +0700

    luan up code api admin

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <form method="POST" action="{{ route('signup') }}">
+        @csrf
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <label for="password_confirmation">Confirm Password:</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" required>
+        </div>
+        <button type="submit">Sign Up</button>
+    </form>
+    <a href="{{ route('login') }}">Login</a>
+</body>
+</html>
