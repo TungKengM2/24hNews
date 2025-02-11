48f5fbd93e53d12519b7b3a35ecaa2a043b26de6
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-11 10:37:01 +0700

    tách menu với list ra dashboard

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -161,17 +161,78 @@ body {
 
 <body>
     <div class="wrapper">
-        @include('admin.menu')
-        @include('admin.listpost')
+        <aside id="sidebar">
+            <div class="d-flex">
+                <div class="sidebar-logo">
+                    <a href="#">logo 24hnews</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span>Người dùng</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Post</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link has-dropdown">
+                        <i class="lni lni-protection"></i>
+                        <span>Chức Năng </span>
+                    </a>
+                    <ul class="sidebar-dropdown list-unstyled">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Chức Năng 1</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Chức Năng 2</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link has-dropdown">
+                        <i class="lni lni-layout"></i>
+                        <span>Liên Kết</span>
+                    </a>
+                    <ul class="sidebar-dropdown list-unstyled">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Liên kết 1</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Liên kết 2</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-popup"></i>
+                        <span>Thông báo</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-cog"></i>
+                        <span>Cài đặt</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Đăng xuất</span>
+                </a>
+            </div>
+        </aside>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-    <script>
-        document.getElementById('menu-toggle').addEventListener('click', function () {
-            document.getElementById('sidebar').classList.toggle('expand');
-        });
-    </script>
+
 </body>
 
 </html>
