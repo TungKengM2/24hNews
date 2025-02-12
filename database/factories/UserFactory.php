bbf7020812ff43d02c2ae9b775e4728a9a052950
SonwTungf <sonwtungf.2004@gmail.com>
2025-02-12 19:05:30 +0700

    fix database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => $this->faker->unique()->phoneNumber,
             'email_verified_at' => now(),
+            'role_id' => $this->faker->numberBetween(1, 3),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
