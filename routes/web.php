d2f5837e3f49a1a5cf6d625a5769bc106020c93d
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-12 11:40:17 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,28 @@ Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup')
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('/verify-otp', [AuthController::class, 'showOtpForm'])->name('otp.verify.form');
+// Route xử lý OTP
+Route::post('/verify-otp', [AuthController::class, 'verifyOtp'])->name('otp.verify.process');
+
+
+// Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+// Hiển thị form nhập email để lấy lại mật khẩu
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])
+    ->name('password.request');
+
+// Xử lý gửi email đặt lại mật khẩu
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
+    ->name('password.email');
+
+// Hiển thị form nhập mật khẩu mới
+Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'showResetForm'])
+    ->name('password.reset');
+
+// Xử lý cập nhật mật khẩu mới
+ Route::post('/reset-password', [ForgotPasswordController::class, 'reset'])
+     ->name('password.update');
