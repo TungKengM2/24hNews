b40651a287046173023a3d9c53d8f7d22977aa38
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-02-13 22:04:34 +0700

    luan code user profile

--- resources/views/client/profile/layouts/partials/content.blade.php
+++ resources/views/client/profile/layouts/partials/content.blade.php
@@ -1,6 +1,19 @@
 <div class="col-md-9">
     <div class="profile-content">
-       Thông Tin Người Dùng
-
+       <h2>Thông Tin Người Dùng</h2>
+       <div class="user-info">
+           <h3>Thông tin cá nhân</h3>
+           <ul class="list-group">
+               <li class="list-group-item"><strong>Họ tên:</strong> Vu Thanh Luan</li>
+               <li class="list-group-item"><strong>Email:</strong> abc@gmail.comcom</li>
+               <li class="list-group-item"><strong>Số điện thoại:</strong> 0123456789</li>
+               <li class="list-group-item"><strong>Địa chỉ:</strong> Cao Đẳng FPT , Trịnh Văn Bô , Nam Từ Liêm , Hà Nội</li>
+           </ul>
+       </div>
+       <div class="user-settings mt-4">
+           <h3>Cài đặt tài khoản</h3>
+           <button class="btn btn-primary">Chỉnh sửa thông tin</button>
+           <button class="btn btn-danger">Đổi mật khẩu</button>
+       </div>
     </div>
 </div>
