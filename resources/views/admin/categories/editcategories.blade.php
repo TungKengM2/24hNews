3adeba16c8fc4a78d43aca50801caf0fd46d7c5c
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-13 19:31:58 +0700

    up

--- resources/views/admin/categories/editcategories.blade.php
+++ resources/views/admin/categories/editcategories.blade.php
@@ -34,10 +34,11 @@
 </head>
 
 <body>
-    <div class="wrapper">
+<div class="wrapper">
     @include('admin.menu')
-    <div class="container mt-5">
-        <h2 class="mb-4">Edit Category</h2>
+    <div class="container mt-5 ">
+       <div class="card p-2">
+       <h2 class="mb-4">Edit Category</h2>
         <form action="/admin/post/store" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-3">
@@ -81,6 +82,7 @@
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
