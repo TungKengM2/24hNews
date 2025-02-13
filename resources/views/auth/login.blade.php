53c238d14108c7fdbf3943b825e74803b2d549e3
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-13 11:57:18 +0700

    giao dien login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,14 @@
 @section('title', 'Đăng nhập')
 
 @section('content')
-    <h2>Đăng nhập</h2>
+<div class="login-container">
+    <div class="row">
+        <div class="col-md-6 ads">
+        <h3>Đăng nhập</h3>
+          <div class="img">
+            <img src="https://i.imgur.com/HWuWWE9.jpeg" alt="">
+          </div>
+        </div>
 
     @if (session('status'))
         <div class="alert alert-success">{{ session('status') }}</div>
@@ -12,41 +19,58 @@
     @if (session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
+        <div class="form-section">
+            <div class="profile-img">
+                <img src="https://i.imgur.com/ZuUExs4.jpeg" alt="profile_img" height="140px" width="140px;">
+            </div>
+            <form action="{{ route('login') }}" method="POST" >
+                @csrf
+                <div class="input-icon">
+                    <i class="fa fa-envelope"></i>
+                    <input type="email" name="email" required value="{{ old('email') }}">
+                </div>
+                <div class="input-icon">
+                    <i class="fa fa-lock"></i>
+                    <input type="password" name="password" required id="password">
+                    <div class="eye">
+                        <i class="fa fa-eye" id="togglePassword"></i>
+                    </div>
+                </div>
 
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <label>Email</label>
-        <input type="email" name="email" required value="{{ old('email') }}">
+                @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
 
-        <label>Mật khẩu</label>
-        <input type="password" name="password" required>
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                    Ghi nhớ đăng nhập
+                </label>
 
-        @error('email')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+                <button type="submit">Đăng nhập</button>
+            </form>
+            <div class="login social">
+                <button class="btn btn-google">
+                    <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Icon"> Login with Google
+                </button>
+                <button class="btn btn-facebook">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook Icon"> Login with Facebook
+                </button>
+            </div>
 
-        <label>
-           <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-          Ghi nhớ đăng nhập
-      </label>
-  
-
-        <button type="submit">Đăng nhập</button>
-    </form>
-    <div class="login social" >
-
-        <button class="btn btn-google" >
-            <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Icon"> Login with Google
-        </button>
-        <button class="btn btn-facebook" >
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook Icon"> Login with Facebook
-        </button>
+            <p><a href="{{ route('password.request') }}">Quên mật khẩu?</a></p>
+            <p>Chưa có tài khoản? <a href="{{ route('signup') }}">Đăng ký</a></p>
+        </div>
     </div>
-
-    <p><a href="{{ route('password.request') }}">Quên mật khẩu?</a></p>
-    <p>Chưa có tài khoản? <a href="{{ route('signup') }}">Đăng ký</a></p>
+</div>
+<script>
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+    const password = document.getElementById('password');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
+</script>
 @endsection
 
 <style>
-   
 </style>
