927e58a6cc0b7347696d9d0d16325af623324cfa
Mx2 <tungteng258@gamil.com>
2025-02-14 08:56:37 +0700

    CRUD Category

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -7,26 +7,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Post</title>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
         body {
             font-family: 'Poppins', sans-serif;
             background-color: #f8f9fa;
         }
+
         .wrapper {
             display: flex;
             margin: 0px;
         }
+
         .container {
             width: 100%;
             background-color: #fff;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            margin-left: 300px; 
+            margin-left: 300px;
         }
+
         .form-label {
             font-weight: 600;
         }
@@ -34,18 +39,23 @@
 </head>
 
 <body>
-<div class="wrapper">
-    @include('admin.menu')
-    <div class="container mt-5 ">
-       <div class="card p-2">
-       <h2 class="mb-4">Edit Category</h2>
-        <form action="/admin/post/store" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" required>
-            </div>
-            <div class="mb-3">
+    <div class="wrapper">
+        @include('admin.menu')
+        <div class="container mt-5 ">
+            <div class="card p-2">
+                <h2 class="mb-4">Create New Category</h2>
+                <form action="{{ route('categories.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="is_active" class="form-label">Is Active</label>
+                        <input type="checkbox" name="is_active" checked>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    {{-- <div class="mb-3">
                 <label for="slug" class="form-label">Slug</label>
                 <input type="text" class="form-control" id="slug" name="slug" required>
             </div>
@@ -80,13 +90,14 @@
                 <label for="thumbnail" class="form-label">Thumbnail</label>
                 <input type="file" class="form-control" id="thumbnail" name="thumbnail" required>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-       </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+            <button type="submit" class="btn btn-primary">Submit</button> --}}
+
+                </form>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
