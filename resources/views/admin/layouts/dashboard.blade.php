b70c8614c1e7f07829e44359a97e8783961c4260
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-02-15 12:14:32 +0700

    luan dàn layout admin

--- resources/views/admin/layouts/dashboard.blade.php
+++ resources/views/admin/layouts/dashboard.blade.php
@@ -11,10 +11,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
-<<<<<<< HEAD
-=======
-
->>>>>>> ab90c292c2e51dcf8c7cf171548ae4975f260007
 </head>
 <body>
     <div class="wrapper">
@@ -23,7 +19,9 @@
 
             <div class="main">
                 @include('admin.layouts.partials.header')
-
+                <div class="content">
+                    @yield('content')
+                </div>
         @include('admin.layouts.partials.sidebar')
             </div>
     </div>
--- resources/views/admin/layouts/partials/menusidebar.blade.php
+++ resources/views/admin/layouts/partials/menusidebar.blade.php
@@ -1,72 +1,4 @@
 <aside id="sidebar">
-<<<<<<< HEAD
-<div class="d-flex">
-    <div class="sidebar-logo">
-        <img src="{{ asset('images/logo.png') }}" class="img-fluid" alt="Logo 24H News">
-    </div>
-</div>
-<ul class="sidebar-nav">
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link">
-            <i class="lni lni-user"></i>
-            <span>Người dùng</span>
-        </a>
-    </li>
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link">
-            <i class="lni lni-agenda"></i>
-            <span>Post</span>
-        </a>
-    </li>
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link has-dropdown">
-            <i class="lni lni-protection"></i>
-            <span>Chức Năng </span>
-        </a>
-        <ul class="sidebar-dropdown list-unstyled">
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">Chức Năng 1</a>
-            </li>
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">Chức Năng 2</a>
-            </li>
-        </ul>
-    </li>
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link has-dropdown">
-            <i class="lni lni-layout"></i>
-            <span>Liên Kết</span>
-        </a>
-        <ul class="sidebar-dropdown list-unstyled">
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">Liên kết 1</a>
-            </li>
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">Liên kết 2</a>
-            </li>
-        </ul>
-    </li>
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link">
-            <i class="lni lni-popup"></i>
-            <span>Thông báo</span>
-        </a>
-    </li>
-    <li class="sidebar-item">
-        <a href="#" class="sidebar-link">
-            <i class="lni lni-cog"></i>
-            <span>Cài đặt</span>
-        </a>
-    </li>
-</ul>
-<div class="sidebar-footer">
-    <a href="#" class="sidebar-link">
-        <i class="lni lni-exit"></i>
-        <span>Đăng xuất</span>
-    </a>
-</div>
-</aside>
-=======
             <div class="d-flex">
                 <div class="sidebar-logo">
                     <img src="{{ asset('images/logo.png') }}" class="img-fluid" alt="Logo 24H News">
@@ -139,4 +71,3 @@
                 </a>
             </div>
         </aside>
->>>>>>> ab90c292c2e51dcf8c7cf171548ae4975f260007
