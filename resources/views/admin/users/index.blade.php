ad2d32c388f8b238b8dfa88ccacc65a8f9ef6bd2
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-15 16:13:25 +0700

    up

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,28 +9,15 @@
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body {
-            font-size: 0.875rem; /* Reduce font size */
-        }
-
-        .table th, .table td {
-            padding: 0.5rem; /* Reduce padding */
-        }
-
-        .btn {
-            padding: 0.25rem 0.5rem; /* Reduce button padding */
-            font-size: 0.75rem; /* Reduce button font size */
-        }
-
-        .page-title {
-            font-size: 1.25rem; /* Reduce page title font size */
-        }
-    </style>
+        <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+   
 </head>
 
 <body>
-    <div class="main">
+   <div class="wrapper">
+        @include('admin.layouts.partials.menusidebar')
+   <div class="main">
+        @include('admin.layouts.partials.header')
         <main class="content">
             <div class="container-fluid">
                 <div class="mb-3">
@@ -67,7 +54,7 @@
                                         <td>{{ $user->phone }}</td>
                                         <td><img src="{{ asset('storage/' . $user->image) }}" alt="{{ $user->username }}" width="100"></td>
                                         <td>{{ $user->email }}</td>
-                                        <td>{{ $user->role }}</td>
+                                        <td>{{ $user->role_id }}</td>
                                         <td>{{ $user->is_promoted }}</td>
                                         <td>{{ $user->violation_count }}</td>
                                         <td>{{ $user->banned_until }}</td>
@@ -96,6 +83,7 @@
             </div>
         </main>
     </div>
+   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
