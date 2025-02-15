b2bd1b3a9da3c3080ec8da1321b9415a633a8f6d
buihien9969 <buihien9969@gmail.com>
2025-02-15 15:49:04 +0700

    refactor(database): simplify RoleSeeder and update role list - Add 'author' role to the list of roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,58 +1,52 @@
 <?php
 
-    namespace Database\Seeders;
+namespace Database\Seeders;
 
-    use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-    use Illuminate\Database\Seeder;
-    use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-    class RoleSeeder extends Seeder
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
+        DB::table('roles')->insert([
+            [
+                'name' => 'admin',
+                'description' => 'Administrator',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-        /**
-         * Run the database seeds.
-         */
-        public function run(): void
-        {
+            [
+                'name' => 'author',
+                'description' => 'Author',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'moderator',
+                'description' => 'Moderator',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-            DB::table('roles')->insert([
-                [
-                    'name' => 'admin',
-                    'description' => 'Administrator',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
+            [
+                'name' => 'user',
+                'description' => 'User',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-                [
-                    'name' => 'article',
-                    'description' => 'Article',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'moderator',
-                    'description' => 'Moderator',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
+            [
+                'name' => 'guest',
+                'description' => 'Guest',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-                [
-                    'name' => 'user',
-                    'description' => 'User',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-
-                [
-                    'name' => 'guest',
-                    'description' => 'Guest',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]
-
-            ]);
-
-        }
-
-
+        ]);
     }
+}
