6f0ddf006f351b38d7787e29cb8f33c4d4b28050
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-16 21:16:14 +0700

    up admin tác giả

---
+++ app/Http/Controllers/Writer/WriterDashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Writer;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WriterDashboard extends Controller
+{
+    public function index()
+    {
+        return view('writer.layouts.dashboard');
+    }
+}
