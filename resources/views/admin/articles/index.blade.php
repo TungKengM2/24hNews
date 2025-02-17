a9c97c049c24f419826c31787eb523faa451692b
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-17 19:45:56 +0700

    up

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -22,9 +22,6 @@ function formatPlainText($text)
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<<<<<<< HEAD
-    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
-=======
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
 
@@ -136,7 +133,6 @@ function formatPlainText($text)
 
         @media (min-width: 768px) {}
     </style>
->>>>>>> 117f085df7c0dcaf3ee9474e30f3434357a2ceed
 </head>
 
 <body>
