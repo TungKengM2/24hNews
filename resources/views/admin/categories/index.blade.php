a2a4d8f9562d5a5c02afc3b9010b7222cf4bd136
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-20 23:13:01 +0700

    up

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -131,27 +131,9 @@
 
 <body>
     <div class="wrapper">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        @include('admin.layouts.partials.menusidebar')
-        <div class="main">
-            @include('admin.layouts.partials.header')
-=======
         @include('admin.menu')
         <main class="content px-3 py-4 w-100">
             @include('admin.header')
->>>>>>> 65459d5776a18e445735a074bd602982137e0024
-=======
-        @include('admin.layouts.partials.menusidebar')
-        <div class="main">
-            @include('admin.layouts.partials.header')
->>>>>>> 465ec0870b172196221404fd58cf493cde162b3c
-=======
-        @include('admin.menu')
-        <main class="content px-3 py-4 w-100">
-            @include('admin.header')
->>>>>>> cfc6db325ba9dcff812deeb0618e648f61af75ee
             <div class="container-fluid">
                 <div class="mb-3">
                     <div class="row">
