3d5bb7deaa8b9776fc53a79ea63944329e8848f6
buihien9969 <buihien9969@gmail.com>
2025-02-21 18:29:50 +0700

    merge from dat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,82 +1,22 @@
 <?php
-<<<<<<< HEAD
-
-    namespace App\Http\Controllers;
-=======
 
 namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Article;
 use Illuminate\Http\Request;
->>>>>>> origin/dat
 
-    use App\Models\User;
-    use App\Models\Article;
-    use App\Models\ArticleLike;
-    use Illuminate\Support\Str;
-    use Illuminate\Http\Request;
-    use App\Models\ArticleHistory;
-    use Illuminate\Support\Facades\Auth;
-
-    class HomeController extends Controller
+class HomeController extends Controller
+{
+    public function index()
     {
-<<<<<<< HEAD
-
-        public function index()
-        {
-            // $articles = Article::latest()->get();
-=======
         // Lấy tối đa 3 bài viết mới nhất
         $latestArticles = Article::latest()->take(3)->get();
->>>>>>> origin/dat
 
-            // return view('welcome', compact('articles'));
-            //        return view('welcome');
-            // Lấy bài viết có nhiều lượt xem và lượt thích nhất
-            $featuredArticle = Article::where('status', 'published')
-                ->orderByDesc('views') // Sắp xếp theo lượt xem giảm dần
-                ->first();
 
-<<<<<<< HEAD
-            // Lấy danh sách bài viết mới nhất
-            $articles = Article::where('status', 'published')->latest()->get();
-
-            return view('welcome', compact('featuredArticle', 'articles'));
-        }
-
-        public function likeArticle($article_id)
-        {
-            $user = auth()->user();
-
-            if (!$user) {
-                return redirect()
-                    ->route('login')
-                    ->with('error', 'Bạn cần đăng nhập để thích bài viết.');
-            }
-
-            $like = ArticleLike::where('article_id', $article_id)
-                ->where('user_id', $user->id)
-                ->first();
-
-            if ($like) {
-                $like->delete();
-                return back()->with('message', 'Bạn đã bỏ thích bài viết.');
-            } else {
-                ArticleLike::create([
-                    'article_id' => $article_id,
-                    'user_id' => $user->id,
-                ]);
-                return back()->with('success', 'Bạn đã thích bài viết!');
-            }
-        }
-
-    }
-=======
         $categoryArticles = Article::where('category_id', 1)->latest()->take(7)->get();
 
         // Truyền dữ liệu bài viết tới view
         return view('website.pages.home.home', compact('latestArticles', 'categoryArticles'));
     }
 }
->>>>>>> origin/dat
