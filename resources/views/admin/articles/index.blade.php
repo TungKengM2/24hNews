c168d387d6234f7f5b22ed105911449c5d380020
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-21 21:26:39 +0700

    up

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -142,71 +142,6 @@
     <main class="content px-3 py-4">
         <div class="container-fluid">
             <div class="mb-3">
-
-                <div class="row">
-                    <div class="col-12 col-md-4 ">
-                        <div class="card border-0">
-                            <div class="card-body py-4">
-                                <h5 class="mb-2 fw-bold">
-                                    Memebers Progress
-                                </h5>
-                                <p class="mb-2 fw-bold">
-                                    $72,540
-                                </p>
-                                <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                    <span class=" fw-bold">
-                                            Since Last Month
-                                        </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 ">
-                        <div class="card  border-0">
-                            <div class="card-body py-4">
-                                <h5 class="mb-2 fw-bold">
-                                    Memebers Progress
-                                </h5>
-                                <p class="mb-2 fw-bold">
-                                    $72,540
-                                </p>
-                                <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                    <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 ">
-                        <div class="card border-0">
-                            <div class="card-body py-4">
-                                <h5 class="mb-2 fw-bold">
-                                    Memebers Progress
-                                </h5>
-                                <p class="mb-2 fw-bold">
-                                    $72,540
-                                </p>
-                                <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                    <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <h3 class="fw-bold fs-4 my-3">Avg. Agent Earnings
-                </h3>
                 <div class="row">
                     <div class="col-12 mb-3">
                         <h1>Thêm mới bài viết</h1>
@@ -243,6 +178,9 @@
                                     <td>{{ $article->article_id }}</td>
                                     <td>{{ $article->title }}</td>
                                     <td>{{ $article->slug }}</td>
+                                    <td>
+                                        {{ Str::limit(formatPlainText($article->content), 50) }}
+                                    </td>
 
                                     {{-- <td>
                                         {{ Str::limit(formatPlainText($article->content), 50) }}
