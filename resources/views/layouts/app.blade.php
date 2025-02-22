e4aa20f3c4c3f4e753ee8af9f3d805bd17972c91
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-22 13:43:40 +0700

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <style>
         body {
             font-family: Arial, sans-serif;
