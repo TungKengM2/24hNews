63ab453f50c5ce63e8186079a84644bc30393f28
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-22 12:54:40 +0700

    sửa giao diện trang chủ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,13 +10,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // Lấy tối đa 3 bài viết mới nhất
-        $latestArticles = Article::latest()->take(3)->get();
-
-
-        $categoryArticles = Article::where('category_id', 1)->latest()->take(7)->get();
-
-        // Truyền dữ liệu bài viết tới view
-        return view('website.pages.home.home', compact('latestArticles', 'categoryArticles'));
+        return view('website.home.index');
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/ckeditor.js', 'public/js');
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', []);
+    .postCss('resources/css/app.css', 'public/css', [])
+    .sass('resources/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets', 'public/assets');
 mix.setPublicPath('public');
