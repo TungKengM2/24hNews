add24089f19d52ed2604c3c7806666651fb04c4e
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-22 13:23:03 +0700

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,7 @@
-const mix = require('laravel-mix');
+import mix from 'laravel-mix';
 
 mix.js('resources/js/ckeditor.js', 'public/js');
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [])
-    .sass('resources/sass/app.scss', 'public/css')
-    .copyDirectory('resources/assets', 'public/assets');
+    .postCss('resources/css/app.css', 'public/css', []);
+
 mix.setPublicPath('public');
