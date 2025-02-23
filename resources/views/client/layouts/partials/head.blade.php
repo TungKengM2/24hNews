86fde053f5a6ef55b1ef3fb599008175e0fe6b57
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-02-23 20:51:10 +0700

    up code

---
+++ resources/views/client/layouts/partials/head.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;500;600;700;800;900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet" />
+
+    <!-- Bootstrap CSS -->
+
+
+
+</head>
--- resources/views/client/layouts/partials/js.blade.php
+++ resources/views/client/layouts/partials/js.blade.php
@@ -1,1 +1,16 @@
-<script src="{{ asset('/theme/client/assets/js/main.js') }}"></script>
+<script src="{{ asset('/theme/client/js/main.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/parallaxie.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/back-to-top.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/boostrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/jquery-3.0.0.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/jquery-migrate-3.0.0.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/jquery.counterup.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/jquery.fancybox.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/lity.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/mixitup.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/pace.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/parallaxie.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/swiper.min.js') }}"></script>
+<script src="{{ asset('/theme/client/js/lib/wow.min.js') }}"></script>
+
