b51422b8b7092db7b9b145f049df03c16cc412cf
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-02-23 20:58:27 +0700

    up code

--- resources/views/client/layouts/partials/head.blade.php
+++ resources/views/client/layouts/partials/head.blade.php
@@ -16,7 +16,18 @@
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
         rel="stylesheet" />
 
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS của folder client -->
+    <link rel="stylesheet" href="{{ asset('client/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/rtl_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/bootstrap.rtl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/icofont.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/jquery.fancybox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/line-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/lity.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/assets/css/lib/swiper.min.css') }}">
 
 
 
