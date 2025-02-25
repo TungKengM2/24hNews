3be19d630ce95bedbd381415bcf1eb4a36336233
SonwTungf <sonwtungf.2004@gmail.com>
2025-02-25 18:46:28 +0700

    Bai duoc duyet moi hien thi o trang chu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,6 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
@@ -10,11 +8,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // Lấy tối đa 3 bài viết mới nhất
-        $latestArticles = Article::latest()->take(3)->get();
+        // Lấy 3 bài viết mới nhất đã được duyệt
+        $latestArticles = Article::where('status', 'Published')
+            ->latest()
+            ->take(3)
+            ->get();
 
-
-        $categoryArticles = Article::where('category_id', 1)->latest()->take(7)->get();
+        // Lấy 7 bài viết theo danh mục (chỉ lấy bài đã duyệt)
+        $categoryArticles = Article::where('category_id', 1)
+            ->where('status', 'Published')
+            ->latest()
+            ->take(7)
+            ->get();
 
         // Truyền dữ liệu bài viết tới view
         return view('website.pages.home.home', compact('latestArticles', 'categoryArticles'));
