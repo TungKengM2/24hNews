2b6e52a21f8e28ab7649055b93bf198c515f0c87
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-26 15:02:54 +0700

    giao dien admin dashboard

--- resources/views/admin/layouts/dashboard.blade.php
+++ resources/views/admin/layouts/dashboard.blade.php
@@ -1,32 +1,91 @@
 <!DOCTYPE html>
-<html>
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 May 2024 07:23:13 GMT -->
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sidebar With Bootstrap</title>
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Dashboard</title>
+
+
+    <link rel="stylesheet" href="{{ asset('admin/css/bootstrap1.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/themefy_icon/themify-icons.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/niceselect/css/nice-select.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/owl_carousel/css/owl.carousel.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/gijgo/gijgo.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/font_awesome/css/all.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/tagsinput/tagsinput.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/datepicker/date-picker.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/vectormap-home/vectormap-2.0.2.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/scroll/scrollable.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/jquery.dataTables.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/responsive.dataTables.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/buttons.dataTables.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/text_editor/summernote-bs4.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/morris/morris.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/vendors/material_icon/material-icons.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/css/metisMenu.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/css/style1.css') }}" />
+<link rel="stylesheet" href="{{ asset('admin/css/colors/default.css') }}" id="colorSkinCSS">
 
 </head>
-<body>
-    <div class="wrapper h-20">
 
-            @include('admin.layouts.partials.menusidebar')
+<body class="crm_body_bg">
 
-            <div class="main">
-                @include('admin.layouts.partials.header')
+    // Sidebar
+    @include('admin.layouts.partials.sidebar')
+    // main
+    @include('admin.layouts.partials.main')
 
-        @include('admin.layouts.partials.sidebar')
-            </div>
-    </div>
-    <div>
-        @include('admin.layouts.partials.footer')
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    // backtop
+    @include('admin.layouts.partials.backtop')
+
+    <script src="{{ asset('admin/js/jquery1-3.4.1.min.js') }}"></script>
+<script src="{{ asset('admin/js/popper1.min.js') }}"></script>
+<script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin/js/metisMenu.js') }}"></script>
+<script src="{{ asset('admin/vendors/count_up/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/chartlist/Chart.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/count_up/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/niceselect/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.flash.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/jszip.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/pdfmake.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/vfs_fonts.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datepicker/datepicker.js') }}"></script>
+<script src="{{ asset('admin/vendors/datepicker/datepicker.en.js') }}"></script>
+<script src="{{ asset('admin/vendors/datepicker/datepicker.custom.js') }}"></script>
+<script src="{{ asset('admin/js/chart.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/chartjs/roundedBar.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/progressbar/jquery.barfiller.js') }}"></script>
+<script src="{{ asset('admin/vendors/tagsinput/tagsinput.js') }}"></script>
+<script src="{{ asset('admin/vendors/text_editor/summernote-bs4.js') }}"></script>
+<script src="{{ asset('admin/vendors/am_chart/amcharts.js') }}"></script>
+<script src="{{ asset('admin/vendors/scroll/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/scroll/scrollable-custom.js') }}"></script>
+<script src="{{ asset('admin/vendors/vectormap-home/vectormap-2.0.2.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/vectormap-home/vectormap-world-mill-en.js') }}"></script>
+<script src="{{ asset('admin/vendors/apex_chart/apex-chart2.js') }}"></script>
+<script src="{{ asset('admin/vendors/apex_chart/apex_dashboard.js') }}"></script>
+<script src="{{ asset('admin/vendors/echart/echarts.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/chart_am/core.js') }}"></script>
+<script src="{{ asset('admin/vendors/chart_am/charts.js') }}"></script>
+<script src="{{ asset('admin/vendors/chart_am/animated.js') }}"></script>
+<script src="{{ asset('admin/vendors/chart_am/kelly.js') }}"></script>
+<script src="{{ asset('admin/vendors/chart_am/chart-custom.js') }}"></script>
+<script src="{{ asset('admin/js/dashboard_init.js') }}"></script>
+<script src="{{ asset('admin/js/custom.js') }}"></script>
+
 </body>
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 May 2024 07:24:00 GMT -->
+
 </html>
