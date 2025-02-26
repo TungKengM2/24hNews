ab2c5955d223d71dc91d16140e3b698c13c278a2
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-26 15:02:54 +0700

    giao dien admin dashboard

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -1,117 +1,229 @@
-
-<body>
-
-
-
-
-        <main class="content px-3 py-4">
-            <div class="container-fluid">
-                <div class="mb-3">
-
-                    <div class="row">
-                        <div class="col-12 col-md-4 ">
-                            <div class="card border-0">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Memebers Progress
-                                    </h5>
-                                    <p class="mb-2 fw-bold">
-                                        $72,540
-                                    </p>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class=" fw-bold">
-                                            Since Last Month
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 ">
-                            <div class="card  border-0">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Memebers Progress
-                                    </h5>
-                                    <p class="mb-2 fw-bold">
-                                        $72,540
-                                    </p>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 ">
-                            <div class="card border-0">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Memebers Progress
-                                    </h5>
-                                    <p class="mb-2 fw-bold">
-                                        $72,540
-                                    </p>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <h3 class="fw-bold fs-4 my-3">Avg. Agent Earnings
-                    </h3>
-                    <div class="row">
-                        <div class="col-12">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr class="highlight">
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td colspan="2">Larry the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+<nav class="sidebar vertical-scroll  ps-container ps-theme-default ps-active-y">
+        <div class="logo d-flex justify-content-between">
+            <a href="index-2.html"><img src="img/logo.png" alt></a>
+            <div class="sidebar_close_icon d-lg-none">
+                <i class="ti-close"></i>
             </div>
-        </main>
-
-
-
-
-
-
-</body>
+        </div>
+        <ul id="sidebar_menu">
+            <li class="mm-active">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/dashboard.svg" alt>
+                    </div>
+                    <span>Dashboard</span>
+                </a>
+                <ul>
+                    <li><a class="active" href="index-2.html">Sales</a></li>
+                    <li><a href="index_2.html">Default</a></li>
+                    <li><a href="index_3.html">Dark Menu</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/2.svg" alt>
+                    </div>
+                    <span>Apps</span>
+                </a>
+                <ul>
+                    <li><a href="editor.html">editor</a></li>
+                    <li><a href="mail_box.html">Mail Box</a></li>
+                    <li><a href="chat.html">Chat</a></li>
+                    <li><a href="faq.html">FAQ</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/3.svg" alt>
+                    </div>
+                    <span>UI Kits</span>
+                </a>
+                <ul>
+                    <li><a href="colors.html">colors</a></li>
+                    <li><a href="Alerts.html">Alerts</a></li>
+                    <li><a href="buttons.html">Buttons</a></li>
+                    <li><a href="modal.html">modal</a></li>
+                    <li><a href="dropdown.html">Droopdowns</a></li>
+                    <li><a href="Badges.html">Badges</a></li>
+                    <li><a href="Loading_Indicators.html">Loading Indicators</a></li>
+                    <li><a href="State_color.html">State color</a></li>
+                    <li><a href="typography.html">Typography</a></li>
+                    <li><a href="datepicker.html">Date Picker</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/4.svg" alt>
+                    </div>
+                    <span>forms</span>
+                </a>
+                <ul>
+                    <li><a href="Basic_Elements.html">Basic Elements</a></li>
+                    <li><a href="Groups.html">Groups</a></li>
+                    <li><a href="Max_Length.html">Max Length</a></li>
+                    <li><a href="Layouts.html">Layouts</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a href="Board.html" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/5.svg" alt>
+                    </div>
+                    <span>Board</span>
+                </a>
+            </li>
+            <li class>
+                <a href="invoice.html" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/6.svg" alt>
+                    </div>
+                    <span>Invoice</span>
+                </a>
+            </li>
+            <li class>
+                <a href="calender.html" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/7.svg" alt>
+                    </div>
+                    <span>Calander</span>
+                </a>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/8.svg" alt>
+                    </div>
+                    <span>Products</span>
+                </a>
+                <ul>
+                    <li><a href="Products.html">Products</a></li>
+                    <li><a href="Product_Details.html">Product Details</a></li>
+                    <li><a href="Cart.html">Cart</a></li>
+                    <li><a href="Checkout.html">Checkout</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/8.svg" alt>
+                    </div>
+                    <span>Icons</span>
+                </a>
+                <ul>
+                    <li><a href="Fontawesome_Icon.html">Fontawesome Icon</a></li>
+                    <li><a href="themefy_icon.html">themefy icon</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/9.svg" alt>
+                    </div>
+                    <span>Animations</span>
+                </a>
+                <ul>
+                    <li><a href="wow_animation.html">Animate</a></li>
+                    <li><a href="Scroll_Reveal.html">Scroll Reveal</a></li>
+                    <li><a href="tilt.html">Tilt Animation</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/10.svg" alt>
+                    </div>
+                    <span>Components</span>
+                </a>
+                <ul>
+                    <li><a href="accordion.html">Accordions</a></li>
+                    <li><a href="Scrollable.html">Scrollable</a></li>
+                    <li><a href="notification.html">Notifications</a></li>
+                    <li><a href="carousel.html">Carousel</a></li>
+                    <li><a href="Pagination.html">Pagination</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/11.svg" alt>
+                    </div>
+                    <span>Table</span>
+                </a>
+                <ul>
+                    <li><a href="data_table.html">Data Tables</a></li>
+                    <li><a href="bootstrap_table.html">Bootstrap</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/12.svg" alt>
+                    </div>
+                    <span>Cards</span>
+                </a>
+                <ul>
+                    <li><a href="basic_card.html">Basic Card</a></li>
+                    <li><a href="theme_card.html">Theme Card</a></li>
+                    <li><a href="dargable_card.html">Draggable Card</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/13.svg" alt>
+                    </div>
+                    <span>Charts</span>
+                </a>
+                <ul>
+                    <li><a href="chartjs.html">ChartJS</a></li>
+                    <li><a href="apex_chart.html">Apex Charts</a></li>
+                    <li><a href="chart_sparkline.html">Chart sparkline</a></li>
+                    <li><a href="am_chart.html">am-charts</a></li>
+                    <li><a href="nvd3_charts.html">nvd3 charts.</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/14.svg" alt>
+                    </div>
+                    <span>Widgets</span>
+                </a>
+                <ul>
+                    <li><a href="chart_box_1.html">Chart Boxes 1</a></li>
+                    <li><a href="profilebox.html">Profile Box</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/15.svg" alt>
+                    </div>
+                    <span>Maps</span>
+                </a>
+                <ul>
+                    <li><a href="mapjs.html">Maps JS</a></li>
+                    <li><a href="vector_map.html">Vector Maps</a></li>
+                </ul>
+            </li>
+            <li class>
+                <a class="has-arrow" href="#" aria-expanded="false">
+                    <div class="icon_menu">
+                        <img src="img/menu-icon/16.svg" alt>
+                    </div>
+                    <span>Pages</span>
+                </a>
+                <ul>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="resister.html">Register</a></li>
+                    <li><a href="error_400.html">Error 404</a></li>
+                    <li><a href="error_500.html">Error 500</a></li>
+                    <li><a href="forgot_pass.html">Forgot Password</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
