d51c96d4af1cc0c731f42f68f031f3c558d1a69f
Luan Nguyen <luan.nguyen25121999@gmail.com>
2025-02-28 16:09:28 +0700

    Thêm loadingpage.blade.php vào Git

---
+++ resources/views/website/layouts/partials/loadingpage.blade.php
@@ -0,0 +1,22 @@
+ <div class="loading-page style1">
+        <div class="lad-cont">
+            <h2 class="loading loading01">
+                <span>2</span>
+                <span>4</span>
+                <span>H</span>
+                <span>N</span>
+                <span>E</span>
+                <span>W</span>
+                <span>S</span>
+              </h2>
+              <small class="loading loading01">
+                  <span>l</span>
+                  <span>o</span>
+                  <span>a</span>
+                  <span>d</span>
+                  <span>i</span>
+                  <span>n</span>
+                  <span>g</span>
+                </small>
+        </div>
+      </div>
