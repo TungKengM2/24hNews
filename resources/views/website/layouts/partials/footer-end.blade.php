234a7d40794e80d9d7a10fc98466c9c7c3f689eb
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-02-28 22:40:41 +0700

    up trang chu demo

---
+++ resources/views/website/layouts/partials/footer-end.blade.php
@@ -0,0 +1,6 @@
+<div class="foot text-center py-4 border-1 border-top brd-gray">
+    <p class="fsz-13px color-666">© 2023 Copyrights by <a href="home-default.html#"
+            class="color-000">Newzin</a>. All Rights
+        Reserved. Developed by <a href="home-default.html#"
+            class="color-000 text-decoration-underline">ThemesCamp</a></p>
+</div>
---
+++ resources/views/website/layouts/partials/top-nav.blade.php
@@ -0,0 +1,79 @@
+<div class="top-navbar style-1">
+    <div class="container p-0">
+        <div class="row align-items-center">
+            <div class="col-lg-4">
+                <div class="date-weather mb-3 mb-lg-0">
+                    <div class="row align-items-center">
+                        <div class="col-6">
+                            <div class="item">
+                                <div class="icon me-3 pt-1">
+                                    <i class="la la-calendar"></i>
+                                </div>
+                                <div class="inf">
+                                    <strong>Monday</strong>
+                                    <p>Nov 25, 2023</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="item">
+                                <div class="icon me-3 pt-1">
+                                    <i class="la la-cloud-sun"></i>
+                                </div>
+                                <div class="inf">
+                                    <strong>32° deg, Cloudy</strong>
+                                    <p>New York</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <a href="home-default.html#" class="logo-brand d-none d-lg-block">
+                    <img src="{{ asset('client/img/logo_home1.png') }}" alt="" class="dark-none">
+                    <img src="{{ asset('client/img/logo_home1_lt.png') }}" alt="" class="light-none">
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <div class="sub-darkLight">
+                    <div class="row text-end align-items-center">
+                        <div class="col-6">
+                            <a href="home-default.html#0"
+                                class="text-uppercase fs-6 border-bottom border-1 border-dark subs">
+                                <i class="la la-envelope fs-5 me-1"></i>
+                                Subscribe
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <div class="darkLight-btn">
+                                <span class="icon active" id="light-icon">
+                                    <i class="la la-sun"></i>
+                                </span>
+                                <span class="icon" id="dark-icon">
+                                    <i class="la la-moon"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nav-subs-card">
+                        <p class="fsz-16px text-uppercase mb-20"> Newsletter </p>
+                        <div class="sub-form">
+                            <div class="form-group">
+                                <span class="icon">
+                                    <i class="la la-envelope"></i>
+                                </span>
+                                <input type="text" class="form-control" placeholder="your email">
+                                <button>subscribe</button>
+                            </div>
+                            <p class="mt-3 color-666 fsz-12px fst-italic">By subscribing, you accepted the
+                                our <a href="home-default.html#0"
+                                    class="color-777 text-decoration-underline fst-normal">Policy</a></p>
+                        </div>
+                        <span class="cls"> <i class="la la-times"></i> </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
