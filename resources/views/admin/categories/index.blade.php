f857a3bf4bbbaf6d660617882325f929324e4f4d
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-03-01 21:43:20 +0700

    up

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -101,8 +101,4 @@
         <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
         <div class="control-sidebar-bg"></div>
     </div>
-<<<<<<< HEAD
 @endsection
-=======
-@endsection
->>>>>>> 4f4bd7cc0ce4f018506921aec4238874f7978459
