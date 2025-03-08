0417382340bf9bdf3e893b50f9e232465d9aafc3
duongduong <mavand42@gmail.com>
2025-03-08 16:34:50 +0700

    fix routeroute

--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,64 @@
 <?php
 
-use App\Models\Article;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ArticleController;
-
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
-
+use App\Http\Controllers\AuthAdminController;
 use App\Http\Controllers\ArticleUserController;
 use App\Http\Controllers\Author\AuthorDashboard;
 use App\Http\Controllers\CategoryUserController;
 
 use App\Http\Controllers\Writer\WriterDashboard;
 use App\Http\Controllers\AdminDashboardController;
+
+use App\Http\Controllers\Author\ProfileController;
+
 use App\Http\Controllers\ForgotPasswordController;
+
 use App\Http\Controllers\Client\UserProfileController;
 
 use App\Http\Controllers\Moderator\UserManagementController;
+
 use App\Http\Controllers\Moderator\ModeratorArticleController;
+
+use App\Http\Controllers\Moderator\ModeratorDashboardController;
+/*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
+
+// admin
+Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
+    ->name('admin.dashboard');
+Route::get(
+    '/admin/role-upgrade-requests',
+    [AdminDashboardController::class, 'roleUpgradeRequests']
+)
+    ->name('admin.user-role-requests');
+Route::post(
+    '/admin/approve-role-upgrade/{approval_id}',
+    [AdminDashboardController::class, 'approveRoleUpgrade']
+)
+    ->name('admin.approve-role-upgrade');
+Route::post(
+    '/admin/reject-role-upgrade/{approval_id}',
+    [AdminDashboardController::class, 'rejectRoleUpgrade']
+)
+    ->name('admin.reject-role-upgrade');
+
 
 // Home
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-
-
-// Admin
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('/admin/role-upgrade-requests', [AdminDashboardController::class, 'roleUpgradeRequests'])->name('admin.user-role-requests');
-    Route::post('/admin/approve-role-upgrade/{approval_id}', [AdminDashboardController::class, 'approveRoleUpgrade'])->name('admin.approve-role-upgrade');
-    Route::post('/admin/reject-role-upgrade/{approval_id}', [AdminDashboardController::class, 'rejectRoleUpgrade'])->name('admin.reject-role-upgrade');
-});
-
-// Article
-Route::prefix('admin')->group(function () {
-    Route::resource('articles', ArticleController::class);
-});
-
-// Category
-Route::prefix('admin')->group(function () {
-    Route::resource('categories', CategoryController::class);
-});
-
-// Authentication
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
-    Route::post('/signup', [AuthController::class, 'processSignup'])->name('signup.process');
-    Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-    Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
-    Route::post('/reset-password', [ForgotPasswordController::class, 'reset'])->name('password.update');
-});
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 // Client Articles
 Route::get('/client/articles/{article_id}', [ArticleUserController::class, 'show'])->name('client.articles.article');
@@ -69,36 +67,220 @@ Route::post('/client/articles/{article_id}/comments', [ArticleUserController::cl
 Route::post('/client/articles/{article_id}/comments/{comment_id}/reply', [ArticleUserController::class, 'storeReplyComment'])->middleware('auth')->name('client.articles.replyComment');
 
 // Client Category
-use App\Http\Controllers\Moderator\ModeratorDashboardController;
+
 
 Route::get('client/category/{categorySlug}', [CategoryUserController::class, 'index'])->name('client.category.show');
 
 
-// Author Routes
-Route::prefix('author')->middleware(['auth', 'role:author'])->group(function () {
-    Route::get('/dashboard', [AuthorDashboard::class, 'index'])->name('author.dashboard');
-    Route::resource('articles', \App\Http\Controllers\Author\ArticleController::class);
-    Route::get('/profile', [ProfileController::class, 'index'])->name('author.profile');
+Route::prefix('admin')->group(function () {
+    Route::resource('articles', ArticleController::class);
 });
 
-// Moderator Routes
-Route::middleware(['auth', 'role:moderator'])->group(function () {
-    Route::get('/moderator/dashboard', [ModeratorDashboardController::class, 'index'])->name('moderator.dashboard');
-    Route::get('/moderator/articles', [ModeratorArticleController::class, 'index'])->name('moderator.articles');
-    Route::post('/approve-upgrade/{approval_id}', [UserManagementController::class, 'approveUpgrade'])->name('approve.upgrade');
-    Route::post('/reject-upgrade/{approval_id}', [UserManagementController::class, 'rejectUpgrade'])->name('reject.upgrade');
+// category
+Route::prefix('admin')->group(function () {
+    Route::resource('categories', CategoryController::class);
 });
 
-// Writer
-Route::get('/writer/dashboard', [WriterDashboard::class, 'index'])->name('writer.dashboard');
+// Middleware guest - Chặn truy cập /login và /signup nếu đã đăng nhập
+Route::middleware('guest')->group(function () {
+    // Đăng nhập & Đăng ký
+    Route::get('/login', [AuthController::class, 'showLoginForm'])
+        ->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/signup', [AuthController::class, 'showSignupForm'])
+        ->name('signup');
+    Route::post('/signup', [AuthController::class, 'processSignup'])
+        ->name('signup.process');
 
-// Upload File
+    // Quên mật khẩu
+    Route::get(
+        '/forgot-password',
+        [ForgotPasswordController::class, 'showLinkRequestForm']
+    )
+        ->name('password.request');
+    Route::post(
+        '/forgot-password',
+        [ForgotPasswordController::class, 'sendResetLinkEmail']
+    )
+        ->name('password.email');
+    Route::get(
+        '/reset-password/{token}',
+        [ForgotPasswordController::class, 'showResetForm']
+    )
+        ->name('password.reset');
+    Route::post(
+        '/reset-password',
+        [ForgotPasswordController::class, 'reset']
+    )
+        ->name('password.update');
+});
+
+// Đăng xuất - chỉ cho phép khi đã đăng nhập
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth');
+
+// Routes for login and signup
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/signup', [AuthController::class, 'showSignupForm'])
+    ->name('signup');
+Route::post('/signup', [AuthController::class, 'processSignup'])
+    ->name('signup.process');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/verify-otp', [AuthController::class, 'showOtpForm'])
+    ->name('otp.verify.form');
+// Route xử lý OTP
+Route::post('/verify-otp', [AuthController::class, 'verifyOtp'])
+    ->name('otp.verify.process');
+
+// Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get(
+        '/admin/dashboard',
+        [AdminDashboardController::class, 'index']
+    )
+        ->name('admin.dashboard');
+});
+
+// Hiển thị form nhập email để lấy lại mật khẩu
+Route::get(
+    '/forgot-password',
+    [ForgotPasswordController::class, 'showLinkRequestForm']
+)
+    ->name('password.request');
+
+// Xử lý gửi email đặt lại mật khẩu
+Route::post(
+    '/forgot-password',
+    [ForgotPasswordController::class, 'sendResetLinkEmail']
+)
+    ->name('password.email');
+
+// Hiển thị form nhập mật khẩu mới
+Route::get(
+    '/reset-password/{token}',
+    [ForgotPasswordController::class, 'showResetForm']
+)
+    ->name('password.reset');
+
+// Xử lý cập nhật mật khẩu mới
+Route::post('/reset-password', [ForgotPasswordController::class, 'reset'])
+    ->name('password.update');
+
+// //////////////////////////test//////////////////////////////
+Route::get('/writer/dashboard', [WriterDashboard::class, 'index'])
+    ->name('writer.dashboard');
+
+Route::get(
+    '/moderator/dashboard',
+    [ModeratorDashboardController::class, 'index']
+)
+    ->name('moderator.dashboard');
+
+Route::get('/user/dashboard', [UserProfileController::class, 'index'])
+    ->name('user.dashboard');
+Route::post(
+    '/upgrade-to-author',
+    [UserProfileController::class, 'requestAuthorRole']
+)
+    ->middleware('auth')
+    ->name('upgrade.to.author');
+//    Route::post('/profile/request-author-role', [ProfileController::class, 'requestAuthorRole'])->name('profile.request-author-role');
+
+Route::get('/author/dashboard', [AuthorDashboard::class, 'index'])
+    ->name('author.dashboard');
+// //////////////////////////////////////////////////////////////////
+
+Route::prefix('author')
+    ->middleware(['auth', 'role:author'])
+    ->group(function () {
+        Route::get('/dashboard', [AuthorDashboard::class, 'index'])
+            ->name('author.dashboard');
+        Route::get('/articles', [
+            \App\Http\Controllers\Author\ArticleController::class,
+            'index',
+        ])->name('author.articles');
+        Route::get('/articles/create', [
+            \App\Http\Controllers\Author\ArticleController::class,
+            'create',
+        ])->name('author.articles.create');
+        Route::post('/articles', [
+            \App\Http\Controllers\Author\ArticleController::class,
+            'store',
+        ])->name('author.articles.store');
+        Route::post(
+            '/articles',
+            [\App\Http\Controllers\Author\ArticleController::class, 'edit']
+        )
+            ->name('author.articles.edit');
+        Route::post('/articles', [
+            \App\Http\Controllers\Author\ArticleController::class,
+            'update',
+        ])->name('author.articles.update');
+        Route::post('/articles', [
+            \App\Http\Controllers\Author\ArticleController::class,
+            'destroy',
+        ])->name('author.articles.destroy');
+
+        Route::get('/profile', [
+            ProfileController::class,
+            'index',
+        ])->name('author.profile');
+    });
+
+Route::post(
+    '/approve-upgrade/{approval_id}',
+    [UserManagementController::class, 'approveUpgrade']
+)
+    ->middleware('moderator')
+    ->name('approve.upgrade');
+
+Route::post(
+    '/reject-upgrade/{approval_id}',
+    [UserManagementController::class, 'rejectUpgrade']
+)
+    ->middleware('moderator')
+    ->name('reject.upgrade');
+
 Route::post('/upload-file', function (Request $request) {
     if ($request->hasFile('upload')) {
         $file = $request->file('upload');
         $filename = time() . '_' . $file->getClientOriginalName();
         $file->move(public_path('uploads'), $filename);
-        return response()->json(['url' => asset('uploads/' . $filename)]);
+
+        return response()->json([
+            'url' => asset('uploads/' . $filename),
+        ]);
     }
     return response()->json(['error' => 'No file uploaded'], 400);
 })->name('upload.file');
+
+// merge dat
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get(
+    '/client/articles/{article_id}',
+    [ArticleUserController::class, 'show']
+)
+    ->name('client.articles.article');
+Route::prefix('admin')->group(function () {
+    Route::resource('users', UserController::class);
+});
+Route::get(
+    '/admin/login',
+    [AuthAdminController::class, 'showLoginAdminForm']
+)
+    ->name('authadmin.login-admin');
+Route::post('/admin/login', [AuthAdminController::class, 'loginadmin'])
+    ->name('admin.login.submit');
+Route::get(
+    '/moderator/articles',
+    [ModeratorArticleController::class, 'index']
+)->name('author.articles');
+    //    Route::get('/writer/articleauthor',
+    //        [ArticleAuthorManagement::class, 'index'])
+    //        ->name('writer.articleauthor');
+    //    Route::get('/writer/author', [WriterAuthorManagement::class, 'index'])
+    //        ->name('writer.author');
