787ec506ff527c93e6e7d3cef004e395bee86ef7
Mx2 <tungteng258@gamil.com>
2025-03-08 17:24:43 +0700

    TungKeng fix lỗi nhỏ khi merge

--- resources/views/author/layouts/partials/js.blade.php
+++ resources/views/author/layouts/partials/js.blade.php
@@ -6,11 +6,11 @@
 <script src="/admin/main/../assets/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
 <script src="/admin/main/../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
 <script src="/admin/main/../assets/vendor_components/zingchart_branded_version/zingchart.min.js"></script>
-<script src="/admin/main/https://www.amcharts.com/lib/4/core.js"></script>
-<script src="/admin/main/https://www.amcharts.com/lib/4/maps.js"></script>
-<script src="/admin/main/https://www.amcharts.com/lib/4/geodata/worldLow.js"></script>
-<script src="/admin/main/https://www.amcharts.com/lib/4/themes/dataviz.js"></script>
-<script src="/admin/main/https://www.amcharts.com/lib/4/themes/animated.js"></script>
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/maps.js"></script>
+<script src="https://www.amcharts.com/lib/4/geodata/worldLow.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/dataviz.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
 
 <!-- CrmX Admin App -->
 <script src="/admin/main/js/template.js"></script>
