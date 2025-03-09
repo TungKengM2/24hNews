ac41fd4e442f3527c40297ef0aaddb7c3a7bdee8
tungkeng <tungteng258@gmail.com>
2025-03-09 11:45:54 +0700

    TungKeng điều chỉnh lại bảng migrate

--- database/migrations/2025_02_09_140320_create_approvals_table.php
+++ database/migrations/2025_02_09_140320_create_approvals_table.php
@@ -13,16 +13,20 @@ return new class extends Migration
     {
         Schema::create('approvals', function (Blueprint $table) {
             $table->id('approval_id');
-            $table->foreignId('article_id')->nullable()->constrained('articles', 'article_id');
-            $table->foreignId('approved_by')->nullable()->constrained('users', 'user_id');
-            $table->enum('type', ['article', 'role_upgrade'])->default('article');
-            $table->string('requested_role')->nullable();
+            $table->unsignedBigInteger('article_id')->nullable();
+            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->enum('type', ['role_upgrade', 'other_type']);
+            $table->string('requested_role');
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
             $table->boolean('auto_reviewed')->default(false);
-            $table->text('remarks');
-            $table->timestamp('created_at')->useCurrent();
-        });
+            $table->text('remarks')->nullable();
 
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('approved_by')->references('user_id')->on('users')->onDelete('set null');
+
+            $table->timestamps();
+        });
     }
 
     /**
