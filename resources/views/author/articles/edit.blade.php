fc05829759f048025b52a2c11feee316c17d7616
Mx2 <tungteng258@gamil.com>
2025-03-09 16:15:38 +0700

    TungKeng merge SonwTungf + chỉnh sửa lại giao diện

--- resources/views/author/articles/edit.blade.php
+++ resources/views/author/articles/edit.blade.php
@@ -2,8 +2,8 @@
 
 @section('head')
     <!-- Style -->
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap"/>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -11,15 +11,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <style>
-        .select2-container--default .select2-selection--multiple .select2-selection__choice {
-            background-color: #c3bebe;
-            color: white;
-            border: 1px solid #c2c2c2;
-            padding: 5px 10px;
-            border-radius: 5px;
-            font-size: 14px;
-        }
-
         .select2-container--default .select2-selection--multiple .select2-selection__choice {
             background-color: #c3bebe;
             color: white;
@@ -52,11 +43,11 @@
                                 </ul>
                             </div>
                         @endif
-                        @if(session('violation_reasons'))
+                        @if (session('violation_reasons'))
                             <div class="alert alert-warning">
                                 <strong>Lý do vi phạm:</strong>
                                 <ul>
-                                    @foreach(session('violation_reasons') as $word => $reason)
+                                    @foreach (session('violation_reasons') as $word => $reason)
                                         <li><strong>{{ $word }}:</strong> {{ $reason }}</li>
                                     @endforeach
                                 </ul>
@@ -89,55 +80,60 @@
                     </div>
                 @endif
 
-                <form action="{{ route('author.articles.update', $article) }}" method="POST"
-                      enctype="multipart/form-data"
-                      id="articleForm">
+                <form action="{{ route('author.articles.update', $article) }}" method="POST" enctype="multipart/form-data"
+                    id="articleForm">
                     @csrf
                     @method('PUT')
-                    <div class="mb-3">
-                        <label for="title" class="form-label">Tiêu đề</label>
-                        <input type="text" class="form-control" id="title" name="title"
-                               value="{{ $article->title }}" required>
+                    <div class="form-group">
+                        <h5>Title:</h5>
+                        <div class="controls">
+                            <input type="text" class="form-control" id="title" name="title"
+                                value="{{ $article->title }}" required>
+                        </div>
+
                     </div>
 
 
-                    <div class="mb-3">
-                        <label for="slug" class="form-label">Slug</label>
-                        <input type="text" class="form-control" id="slug" name="slug"
-                               value="{{ $article->slug }}" required>
+                    <div class="form-group">
+                        <h5>Slug:</h5>
+                        <div class="controls">
+                            <input type="text" class="form-control" id="slug" name="slug"
+                                value="{{ $article->slug }}" required>
+                        </div>
                     </div>
+
 
                     @php
                         $content = str_replace('src="../../storage', 'src="/storage', $article->content);
                     @endphp
-                    {{--                    <div class="mb-3">--}}
-                    {{--                        <label for="content" class="form-label">Nội dung</label>--}}
-                    {{--                        <textarea id="full-featured" name="content"--}}
-                    {{--                                  class="form-control">{!! $content !!}</textarea>--}}
-                    {{--                    </div>--}}
 
-                    <div class="mb-3">
-                        <label for="content" class="form-label">Nội dung</label>
-                        @if(session('violations'))
+
+                    {{--                    <div class="mb-3"> --}}
+                    {{--                        <label for="content" class="form-label">Nội dung</label> --}}
+                    {{--                        <textarea id="full-featured" name="content" --}}
+                    {{--                                  class="form-control">{!! $content !!}</textarea> --}}
+                    {{--                    </div> --}}
+
+                    <div class="form-group">
+                        <label for="content" class="form-label" style="color: white">Nội dung</label>
+                        @if (session('violations'))
                             <textarea id="full-featured" name="content"
-                                      style="height: 800px; background: #ffe6e6; padding: 10px; border: 1px solid red;">
+                                style="height: 800px; background: #ffe6e6; padding: 10px; border: 1px solid red;">
                             {!! highlightWords(old('content'), session('violations')) !!}
                         </textarea>
                         @else
-                            <textarea id="full-featured" name="content"
-                                      style="height: 800px;">
+                            <textarea id="full-featured" name="content" style="height: 800px;">
                         {!! $content !!}
                         </textarea>
                         @endif
                     </div>
 
 
-                    <div class="mb-3">
-                        <label class="form-label">Chọn hoặc thêm tags:</label>
+                    <div class="form-group">
+                        <h5>Chọn hoặc thêm tags:</h5>
                         <select name="tags[]" class="form-control select2" multiple="multiple">
                             @foreach ($tags as $tag)
-                                <option value="{{ $tag->tag_id }}"
-                                        @if (in_array($tag->tag_id, $selectedTags)) selected @endif>
+                                <option value="{{ $tag->tag_id }}" @if (in_array($tag->tag_id, $selectedTags)) selected @endif>
                                     {{ $tag->name }}
                                 </option>
                             @endforeach
@@ -145,8 +141,8 @@
                     </div>
 
 
-                    <div class="mb-3">
-                        <label class="form-label">Danh mục</label>
+                    <div class="form-group">
+                        <h5>Danh Mục</h5>
                         <select name="category_id" class="form-control">
                             <option value="">-- Không có danh mục --</option> <!-- Tùy chọn không có danh mục -->
                             @foreach ($categories as $category)
@@ -168,23 +164,24 @@
 
                     <input type="hidden" name="author_id" value="{{ $article->author_id }}">
 
-                    <div class="mt-3">
-                        <label class="form-label" for="thumbnail_url">Ảnh Đại Diện</label>
+                    <div class="form-group">
+                        <h5>Ảnh Đại Diện</h5>
                         <input class="form-control" type="file" name="thumbnail_url" id="thumbnail_url">
                         @if ($article->thumbnail_url)
                             <img src="{{ asset('storage/' . $article->thumbnail_url) }}" alt="Current Thumbnail"
-                                 width="100">
+                                width="100">
                         @endif
                     </div>
 
                     <input type="hidden" name="status" id="articleStatus" value="{{ $article->status }}">
-                    <button type="button" class="btn btn-secondary" id="saveDraft">Lưu nháp</button>
-                    <button type="submit" class="btn btn-primary mt-3">Cập nhật</button>
+                        <button type="button" class="btn btn-secondary" id="saveDraft">Lưu nháp</button>
+                        <button type="submit" class="btn btn-primary">Cập nhật</button>
+
                 </form>
             </div>
 
             <script>
-                $(document).ready(function () {
+                $(document).ready(function() {
                     $('.select2').select2({
                         tags: true,
                         tokenSeparators: [','],
@@ -193,7 +190,7 @@
                     });
                 });
 
-                document.getElementById('title').addEventListener('input', function () {
+                document.getElementById('title').addEventListener('input', function() {
                     let title = this.value.trim();
                     let slug = title.toLowerCase()
                         .normalize('NFD').replace(/[̀-ͯ]/g, '')
@@ -208,16 +205,16 @@
             </script>
             <script>
                 // Lưu nháp
-                document.getElementById('saveDraft').addEventListener('click', function () {
+                document.getElementById('saveDraft').addEventListener('click', function() {
                     document.getElementById('articleStatus').value = 'draft';
                     document.getElementById('articleForm').submit();
                 });
 
-                document.getElementById('articleForm').addEventListener('submit', function () {
+                document.getElementById('articleForm').addEventListener('submit', function() {
                     if (document.activeElement.id !== 'saveDraft') {
                         document.getElementById('articleStatus').value = 'pending';
                     }
                 });
             </script>
 
-@endsection
+        @endsection
