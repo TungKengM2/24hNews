2a071d5b90b0987b0d22b2006e951dccd27717e1
buihien9969 <buihien9969@gmail.com>
2025-03-09 18:11:20 +0700

    refactor(migrations): update approvals table migrations
    - Add user_id and nullable remarks to approvals table
    - Add updated_at timestamp to approvals table

--- database/migrations/2025_03_07_042831_add_updated_at_to_approvals_table.php
+++ database/migrations/2025_03_07_042831_add_updated_at_to_approvals_table.php
@@ -1,31 +1,30 @@
 <?php
 
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-    return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('approvals', function (Blueprint $table) {
+            $table->timestamp('updated_at')
+                ->nullable()
+                ->after('created_at');
+        });
+    }
 
-        /**
-         * Run the migrations.
-         */
-        public function up(): void
-        {
-            // Schema::table('approvals', function (Blueprint $table) {
-            //     $table->timestamp('updated_at')
-            //         ->nullable()
-            //         ->after('created_at');
-            // });
-        }
-
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void
-        {
-            // Schema::table('approvals', function (Blueprint $table) {
-            //     $table->dropColumn('updated_at');
-            // });
-        }
-
-    };
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('approvals', function (Blueprint $table) {
+            $table->dropColumn('updated_at');
+        });
+    }
+};
