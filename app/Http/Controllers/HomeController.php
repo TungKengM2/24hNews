9f7edc4a8c87d575a8e12acc4f789c499ee05e7d
duongduong <mavand42@gmail.com>
2025-03-10 13:52:48 +0700

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Article;
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Catch_;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // breaking news
+        //breaking news
         $featuredArticles = Article::where('status', 'published')
             ->orderByDesc('created_at') // Sắp xếp theo thời gian mới nhất
             ->take(7)
             ->get();
 
-        // top 2 bài viết nhiều lượt xem
+        //top 2 bài viết nhiều lượt xem
         $D1Articles = Article::where('status', 'published')
             ->orderByDesc('views') // Sắp xếp bài viết nhiều views nhất
             ->take(2) // Lấy top 2 bài viết nhiều lượt xem
@@ -24,25 +28,25 @@ class HomeController extends Controller
 
         // Lấy danh sách bài viết mới nhất
         $articles = Article::where('status', 'published')->latest()->get();
-        //
-        //        $trendingPosts = Article::select('articles.*')
-        //            ->join('comments', 'articles.article_id', '=',
-        //                'comments.article_id')
-        //            ->selectRaw('COUNT(comments.comment_id) as comment_count')
-        //            ->groupBy('articles.article_id')
-        //            ->orderByDesc('comment_count')
-        //            ->limit(5)
-        //            ->get();
 
-        $journalists = User::where('role_id', 3)
-            ->get(); // Lấy nhà báo có ID = 3
+        $trendingPosts = Article::select('articles.*')
+            ->join('comments', 'articles.article_id', '=', 'comments.article_id')
+            ->selectRaw('COUNT(comments.comment_id) as comment_count')
+            ->groupBy('articles.article_id')
+            ->orderByDesc('comment_count')
+            ->limit(5)
+            ->get();
+
+        $journalists = User::where('role_id', 3)->get(); // Lấy nhà báo có ID = 3
+
 
         $sportsArticles = Article::whereHas('category', function ($query) {
-            $query->where('name',
-                'Thể Thao'); // Hoặc sử dụng category_id cụ thể
+            $query->where('name', 'Thể Thao'); // Hoặc sử dụng category_id cụ thể
         })->inRandomOrder()->distinct()->get();
 
-        // 11111111
+
+
+        //11111111
         $categories = Category::where('is_active', 1)->get();
         $newsData = [];
         foreach ($categories as $category) {
@@ -54,17 +58,29 @@ class HomeController extends Controller
             if ($article) {
                 $newsData[] = [
                     'category' => $category,
-                    'article' => $article,
+                    'article' => $article
                 ];
             }
         }
 
         $categories = Category::where('is_active', 1)->get();
-
+                
         // Truyền dữ liệu bài viết tới view
-        return view('welcome',
-            compact('categories', 'sportsArticles', 'newsData',
-                'journalists', 'featuredArticles',
-                'articles', 'D1Articles'));
+        return view('welcome', compact('categories','sportsArticles', 'newsData', 'journalists', 'trendingPosts', 'featuredArticles', 'articles', 'D1Articles'));
     }
+    
+    
 }
+    
+    
+    
+
+
+    
+
+
+  
+    
+    
+    
+
