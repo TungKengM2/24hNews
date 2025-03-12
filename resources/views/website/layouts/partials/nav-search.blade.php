328901bb3b3003ea166bb94bc67a5f200a009c70
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-03-12 15:07:36 +0700

    up

--- resources/views/website/layouts/partials/nav-search.blade.php
+++ resources/views/website/layouts/partials/nav-search.blade.php
@@ -29,6 +29,7 @@
                     <ul>
                         @foreach($results as $result)
                             <li>{{ $result->title }} 
+                                <a href="{{ Auth::check() ? route('client.articles.article', $result->article_id) : url('/login-user') }}" class="btn btn-sm btn-primary">Xem</a>
                             </li>
                         @endforeach
                     </ul>
