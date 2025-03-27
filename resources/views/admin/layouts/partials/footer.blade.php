d9b201dff258cfd3c7c321fe2009c7ff2f3c404a
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-03-27 15:04:09 +0700

    up

--- resources/views/admin/layouts/partials/footer.blade.php
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="pull-right d-none d-sm-inline-block">
         <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
             <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+                <a class="nav-link" >FAQ</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="index.html#">Purchase Now</a>
+                <a class="nav-link">Purchase Now</a>
             </li>
         </ul>
     </div>
-    &copy; 2021 <a href="https://www.multipurposethemes.com/">Multipurpose Themes</a>. All Rights Reserved.
+    &copy; 2021 Multipurpose Themes. All Rights Reserved.
 </footer>
