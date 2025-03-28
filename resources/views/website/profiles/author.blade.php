222d9f89475dd5174540e8676fec2aa7b17b1139
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-03-28 22:32:05 +0700

    up

--- resources/views/website/profiles/author.blade.php
+++ resources/views/website/profiles/author.blade.php
@@ -143,83 +143,87 @@
                 </div>
             </div>
         </section>
-               <!-- ====== start modals ====== -->
+                      <!-- ====== start modals ====== -->
 
-               <div class="offcanvas offcanvas-start sidebar-popup-style1" tabindex="-1" id="offcanvasExample"
-               aria-labelledby="offcanvasExampleLabel">
-               <div class="offcanvas-header">
-                   <div class="logo">
-                       <img src="client/assets/img/logo_home1.png" alt="" class="dark-none">
-                       <img src="client/assets/img/logo_home1_lt.png" alt="" class="light-none">
-                   </div>
-                   <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                       aria-label="Close"></button>
-               </div>
-               <div class="offcanvas-body mt-4">
-                   <h6 class="color-000 text-uppercase mb-15 ltspc-1"> about us <i class="la la-angle-right ms-1"></i>
-                   </h6>
-                   <div class="text">
-                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem optio tempora quia iure quae.
-                       Soluta corporis quidem aperiam amet nihil.
-                   </div>
-   
-                   <div class="sidebar-categories mt-40">
-                       <h6 class="color-000 text-uppercase mb-30 ltspc-1"> categories <i class="la la-angle-right ms-1"></i>
-                       </h6>
-   
-                       @foreach ($category2 as $category)
-                           <a href="{{ route('client.category.show', $category->slug) }}" class="cat-card">
-                               <div class="img img-cover " >
-                                   
-                                   <div class="info">
-                                       <h5 href="{{ route('client.category.show', $category->slug) }}">
-                                           {{ $category->name }}
-                                       </h5>
-                                       <span class="num">{{ $loop->iteration }}</span> <!-- Số thứ tự danh mục -->
-                                   </div>
-                               </div>
-                           </a>
-                       @endforeach
-   
-                       
-                   </div>
-                   <div class="sidebar-contact-info mt-50">
-                       <h6 class="color-000 text-uppercase mb-20 ltspc-1"> Contact & follow <i
-                               class="la la-angle-right ms-1"></i></h6>
-                       <ul class="m-0">
-                           <li class="mb-3">
-                               <i class="las la-map-marker me-2 color-main fs-5"></i>
-                               <a href="home-default.html#">streat name 12, hollywood City, USA</a>
-                           </li>
-                           <li class="mb-3">
-                               <i class="las la-envelope me-2 color-main fs-5"></i>
-                               <a href="home-default.html#">Newzin@gmail.com</a>
-                           </li>
-                           <li class="mb-3">
-                               <i class="las la-phone-volume me-2 color-main fs-5"></i>
-                               <a href="home-default.html#">+12 123 456 789</a>
-                           </li>
-                       </ul>
-                       <div class="social-links">
-                           <a href="home-default.html#">
-                               <i class="la la-twitter"></i>
-                           </a>
-                           <a href="home-default.html#">
-                               <i class="la la-facebook-f"></i>
-                           </a>
-                           <a href="home-default.html#">
-                               <i class="la la-instagram"></i>
-                           </a>
-                           <a href="home-default.html#">
-                               <i class="la la-youtube"></i>
-                           </a>
-                           <a href="home-default.html#">
-                               <i class="la la-spotify"></i>
-                           </a>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           <!-- ====== end modals ====== -->
+        <div class="offcanvas offcanvas-start sidebar-popup-style1" tabindex="-1" id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+            <div class="logo">
+                <h1>News24h</h1>
+            </div>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body mt-4">
+            <h6 class="color-000 text-uppercase mb-15 ltspc-1 fw-bold"> Giới Thiệu News24h <i class="la la-angle-right ms-1"></i>
+            </h6>
+            <div class="text mb-4">
+                News24h là nền tảng tin tức hàng đầu Việt Nam, cung cấp thông tin chính xác, đa dạng và cập nhật 24/7. 
+                Chúng tôi cam kết mang đến cho độc giả những tin tức chất lượng và đáng tin cậy từ mọi lĩnh vực.
+            </div>
+
+            <div class="mt-4">
+                <h6 class="color-000 mb-3 fw-bold">Tại sao chọn News24h?</h6>
+                <div class="d-flex align-items-center mb-3">
+                    <div class="icon-box me-3 bg-light rounded p-2" style="color: var(--bs-primary);">
+                        <i class="la la-newspaper-o text-primary"></i>
+                    </div>
+                    <div>
+                        <p class="mb-0">Tin tức chính xác, đa chiều</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-3">
+                    <div class="icon-box me-3 bg-light rounded p-2" style="color: var(--bs-primary);">
+                        <i class="la la-bolt text-primary"></i>
+                    </div>
+                    <div>
+                        <p class="mb-0">Cập nhật tin tức 24/7</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-4">
+                    <div class="icon-box me-3 bg-light rounded p-2" style="color: var(--bs-primary);">
+                        <i class="la la-users text-primary"></i>
+                    </div>
+                    <div>
+                        <p class="mb-0">Cộng đồng độc giả lớn mạnh</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sidebar-contact-info mt-4 pt-4 border-top">
+                <h6 class="color-000 text-uppercase mb-20 ltspc-1 fw-bold"> Liên Hệ & Theo Dõi <i
+                        class="la la-angle-right ms-1"></i> </h6>
+                <ul class="m-0">
+                    <li class="mb-3">
+                        <i class="las la-map-marker me-2 color-main fs-5"></i>
+                        <a href="#">Tòa nhà FPT Polytechnic., Cổng số 2, 13 P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội</a>
+                    </li>
+                    <li class="mb-3">
+                        <i class="las la-envelope me-2 color-main fs-5"></i>
+                        <a href="#">bayanhtai@gmail.com</a>
+                    </li>
+                    <li class="mb-3">
+                        <i class="las la-phone-volume me-2 color-main fs-5"></i>
+                        <a href="#">0981 725 836</a>
+                    </li>
+                </ul>
+                <div class="social-links mt-3">
+                    <a href="#" class="me-2">
+                        <i class="la la-twitter"></i>
+                    </a>
+                    <a href="#" class="me-2">
+                        <i class="la la-facebook-f"></i>
+                    </a>
+                    <a href="#" class="me-2">
+                        <i class="la la-instagram"></i>
+                    </a>
+                    <a href="#" class="me-2">
+                        <i class="la la-youtube"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ====== end modals ====== -->
     </main>
 @endsection
