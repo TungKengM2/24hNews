d939fcf885a11634a2446341250ac26411654d36
tungkeng <tungteng258@gmail.com>
2025-03-28 17:05:57 +0700

    TungKeng thêm chức năng cho admin + chỉnh sửa lại giao diện

--- resources/views/moderator/saved.blade.php
+++ resources/views/moderator/saved.blade.php
@@ -53,7 +53,7 @@
                                                 <h5 class="card-title">{{ $savedArticle->article->title }}</h5>
                                             </td>
                                             <td>
-                                                <p class="card-text">{!! Str::limit($savedArticle->article->content, 100) !!}</p>
+                                                {!! Str::limit(strip_tags($savedArticle->article->content), 100, '...') !!}
                                             </td>
                                             <td>
                                                 <h5 class="card-title">{{ $savedArticle->created_at->diffForHumans() }}
--- resources/views/website/layouts/partials/start-nav.blade.php
+++ resources/views/website/layouts/partials/start-nav.blade.php
@@ -23,41 +23,30 @@
                         <i class="la la-home fs-4"></i>
                     </a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="page-author.html#" id="navbarDropdown1" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Blog
-                    </a>
-                    <ul class="dropdownMenu" aria-labelledby="navbarDropdown1">
-                        <li><a class="dropdown-item" href="page-blog.html">Blog</a></li>
-                        <li><a class="dropdown-item" href="{{url('/profile')}}">authors</a></li>
-                        <li><a class="dropdown-item" href="page-author-details.html">author details</a></li>
-                    </ul>
-                </li>
 
                 {{-- dat them --}}
                 @foreach ($categories as $category)
-                @if ($category->is_active == 1)
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('client.category.show', $category->slug) }}">
-                            {{ $category->name }}
-                        </a>
-                    </li>
-                @endif
+                    @if ($category->is_active == 1)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('client.category.show', $category->slug) }}">
+                                {{ $category->name }}
+                            </a>
+                        </li>
+                    @endif
                 @endforeach
-                 {{-- dat them --}}
+                {{-- dat them --}}
             </ul>
 
-                <div class="nav-side">
-                    <a href="{{ route('loginuser') }} " class="icon-link">
-                        <i class="la la-user fs-4">
-                        </i>
-                    </a>
-                    <a class="icon-link search-btn-style1">
-                        <i class="la la-search fs-4 sOpen-btn"></i>
-                        <i class="la la-close fs-4 sClose-btn"></i>
-                    </a>
-                </div>
+            <div class="nav-side">
+                <a href="{{ route('loginuser') }} " class="icon-link">
+                    <i class="la la-user fs-4">
+                    </i>
+                </a>
+                <a class="icon-link search-btn-style1">
+                    <i class="la la-search fs-4 sOpen-btn"></i>
+                    <i class="la la-close fs-4 sClose-btn"></i>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
