c56d2d38aeaac6c11cef35504b23cf80a4a80e6e
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-03-28 17:55:04 +0700

    up

--- resources/views/website/layouts/partials/footer-main.blade.php
+++ resources/views/website/layouts/partials/footer-main.blade.php
@@ -1,218 +1,62 @@
 <div class="content">
     <div class="row gx-lg-5">
-        <div class="col-lg-4">
+        <div class="col-lg-12">
             <div class="info">
-                <div class="foot-logo">
-                    <img src="assets/img/logo_home1.png" alt="" class="dark-none">
-                    <img src="assets/img/logo_home1_lt.png" alt="" class="light-none">
+                <div class="row align-items-center mb-4">
+                    <div class="col-lg-3">
+                        <div class="foot-logo">
+                            <img src="assets/img/logo_home1.png" alt="" class="dark-none">
+                            <img src="assets/img/logo_home1_lt.png" alt="" class="light-none">
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="row">
+                            <div class="col d-flex">
+                                <p class="me-4">
+                                    <b>Vận hành bởi: </b> Bảy Anh Tài
+                                </p>
+                                <p class="me-4">
+                                    <b>Mã số thuế: </b> 0514234567
+                                </p>
+                                <p>
+                                    <b>Địa chỉ: </b> FPT POLYTECHNIX Hà Nội
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col d-flex">
+                                <p class="me-4">
+                                    <b>Email: </b> support@Newzin.com
+                                </p>
+                                <p class="me-4">
+                                    <b>Điện thoại: </b> (+051) 3235 68 69
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="data-info">
-                    <p>
-                        <b>Vận hành bởi: </b> Bảy Anh Tài
-                    </p>
-                    <p>
-                        <b>Mã số thuế: </b> 0514234567
-                    </p>
-                    <p>
-                        <b>Địa chỉ: </b> FPT POLYTECHNIX  <br>Hà Nội
-
-                    </p>
-                    <p>
-                        <b>Email: </b> support@Newzin.com
-                    </p>
-                    <p>
-                        <b>Điện thoại: </b> (+051) 3235 68 69
-                    </p>
-                    <p>
-                        Giấy phép số 12/AB-BCDEF, ký ngày <br> 29 tháng 1, 2022
-                    </p>
-                </div>
-
-                <div class="social-links">
-                    <a href="#">
-                        <i class="la la-twitter"></i>
-                    </a>
-                    <a href="#">
-                        <i class="la la-facebook-f"></i>
-                    </a>
-                    <a href="#">
-                        <i class="la la-instagram"></i>
-                    </a>
-                    <a href="#">
-                        <i class="la la-youtube"></i>
-                    </a>
-                    <a href="#">
-                        <i class="la la-spotify"></i>
-                    </a>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="la la-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="la la-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="la la-instagram"></i>
+                            </a>
+                            <a href="#">
+                                <i class="la la-youtube"></i>
+                            </a>
+                            <a href="#">
+                                <i class="la la-spotify"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-lg-2">
-            <div class="link-group">
-                <div class="title">Kinh doanh</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Khởi nghiệp</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Xu hướng kinh doanh</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Văn hóa làm việc</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Tài chính cá nhân</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Lãnh đạo</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Thị trường chứng khoán</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="link-group">
-                <div class="title">Chính trị</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Nhật ký chính trị</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">An ninh quốc gia</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Video chính trị</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div class="link-group">
-                <div class="title">Công nghệ</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Công nghệ cá nhân</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Đánh giá sản phẩm</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Trò chơi điện tử</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Khám phá</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Kiến thức</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="link-group">
-                <div class="title">Phong cách sống</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Xu hướng phong cách sống</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Gia đình</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Tâm lý học</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Thời trang</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Ẩm thực</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Văn hóa</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div class="link-group">
-                <div class="title">Giải trí</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Âm nhạc & Podcast</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Nghệ thuật & Thiết kế</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Phim ảnh</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="link-group">
-                <div class="title">Thể thao</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Bóng đá</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Quần vợt</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Giải đua F1</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">NBA</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Bóng rổ</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div class="link-group">
-                <div class="title">Du lịch</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Khám phá</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Vé máy bay & Khách sạn</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Tour du lịch</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Điểm đến</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Trải nghiệm & Đánh giá</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="link-group">
-                <div class="title">Trung tâm hỗ trợ</div>
-                <ul>
-                    <li>
-                        <a href="#" class="f-link">Về chúng tôi</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Liên hệ</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Chương trình đối tác</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Chính sách bảo mật</a>
-                    </li>
-                    <li>
-                        <a href="#" class="f-link">Điều khoản & Điều kiện</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('loginadmin') }}" class="f-link">Quản trị viên</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
     </div>
 </div>
