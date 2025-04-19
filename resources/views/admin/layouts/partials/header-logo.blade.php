37b4c807b80b76571547c3c41cbcf8338e702b95
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-04-19 17:17:17 +0700

    up logo

--- resources/views/admin/layouts/partials/header-logo.blade.php
+++ resources/views/admin/layouts/partials/header-logo.blade.php
@@ -2,12 +2,6 @@
     <!-- Logo -->
     <a href="{{ url('/') }}" class="logo">
         <!-- logo-->
-        <div class="logo-mini w-30">
-            <span class="light-logo"><img class="image"  src="/admin/main/../images/logo-letter.png" alt="logo"></span>
-            <span class="dark-logo"><img class="image"  src="/admin/main/../images/logo-letter.png" alt="logo"></span>
-        </div>
-        <div class="logo-lg mt-1">
-            <h2>News24h</h2>
-        </div>
+            <img src="{{ asset('images/adminlogo.png') }}" alt="logo"  >
     </a>
 </div>
