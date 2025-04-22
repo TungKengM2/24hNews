13e3d80d01955191e1fadefd7636c897f714ca6d
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2025-04-22 18:12:02 +0700

    up

--- app/Models/News.php
+++ app/Models/News.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Models;
+// namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
-{
-    protected $fillable = ['title', 'category', 'content'];
-}
+// class News extends Model
+// {
+//     protected $fillable = ['title', 'category', 'content'];
+// }
