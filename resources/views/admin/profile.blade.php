1b51559ea2b8a2771fc919a159d1008860942b51
tungkeng <tungteng258@gmail.com>
2025-04-25 18:10:45 +0700

    Tungkeng sửa lại chút css giao diện pro5

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -16,9 +16,9 @@
                     <div class="user-profile">
                         <div class="box box-widget widget-user">
                             <div class="widget-user-header bg-img bbsr-0 bber-0 justify-content-center align-items-center h-175 pl-[25px] flex-block  "
-                                style="background: url('../images/gallery/full/10.jpg') center center;" data-overlay="5">
-                                <h1 class="widget-user-username text-white">{{ $user->username }}</h1>
-                                <h4 class="widget-user-desc text-white mt-2">{{ $user->description ?? 'Chưa có mô tả' }}</h4>
+                                style="background: url('/admin/images/gallery/full/10.jpg') center center;" data-overlay="5">
+                                <h1 class="widget-user-username text-white" style="margin-right: 50px">{{ $user->username }}</h1>
+                                <h4 class="widget-user-desc text-white mt-2" style="margin-right: 50px">{{ $user->description ?? 'Chưa có mô tả' }}</h4>
                             </div>
                             <div class="author-img">
                                 <div class="widget-user-image">
